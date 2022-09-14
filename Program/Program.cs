f5e3a0fa291b30506d21862980a076957b6fcb44
Rumyanceva <nj89.89@mail.ru>
2022-09-14 19:57:29 +0300

    Add FillStringArray and PrintArray methods in program

---
+++ Program/Program.cs
@@ -0,0 +1,28 @@
+﻿string [] array = new string [10];
+
+string [] FillStringArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i = i + 2)
+    {
+        int temp = new Random().Next(-100, 100);
+        array[i] = Convert.ToString(temp);
+    }
+    for(int i = 1; i < array.Length; i = i + 2)
+    {
+       System.Random temp = new Random(Guid.NewGuid().GetHashCode());
+       array[i] = Convert.ToString(Convert.ToChar(Convert.ToInt32(Math.Floor(26 * temp.NextDouble() + 65))));
+    }
+    return array;
+}
+
+void PrintArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+FillStringArray(array);
+PrintArray(array);
